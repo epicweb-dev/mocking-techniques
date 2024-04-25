da0860f37f8a09384ff5a23f2d36887bb92e7d23
Artem Zakharchenko <kettanaito@gmail.com>
2024-04-25 13:49:04 +0200

    structure the outlined exercises

--- scripts/fix.js
+++ scripts/fix.js
@@ -11,31 +11,31 @@ const VERBOSE = false
 const logVerbose = (...args) => (VERBOSE ? console.log(...args) : undefined)
 
 const workshopRoot = here('..')
-const examples = (await readDir(here('../examples'))).map(dir =>
-	here(`../examples/${dir}`),
+const examples = (await readDir(here('../examples'))).map((dir) =>
+  here(`../examples/${dir}`),
 )
 const exercises = (await readDir(here('../exercises')))
-	.map(name => here(`../exercises/${name}`))
-	.filter(filepath => fs.statSync(filepath).isDirectory())
+  .map((name) => here(`../exercises/${name}`))
+  .filter((filepath) => fs.statSync(filepath).isDirectory())
 const exerciseApps = (
-	await Promise.all(
-		exercises.flatMap(async exercise => {
-			return (await readDir(exercise))
-				.filter(dir => {
-					return /(problem|solution)/.test(dir)
-				})
-				.map(dir => path.join(exercise, dir))
-		}),
-	)
+  await Promise.all(
+    exercises.flatMap(async (exercise) => {
+      return (await readDir(exercise))
+        .filter((dir) => {
+          return /(problem|solution)/.test(dir)
+        })
+        .map((dir) => path.join(exercise, dir))
+    }),
+  )
 ).flat()
-const exampleApps = (await readDir(here('../examples'))).map(dir =>
-	here(`../examples/${dir}`),
+const exampleApps = (await readDir(here('../examples'))).map((dir) =>
+  here(`../examples/${dir}`),
 )
 const apps = [...exampleApps, ...exerciseApps]
 
-const appsWithPkgJson = [...examples, ...apps].filter(app => {
-	const pkgjsonPath = path.join(app, 'package.json')
-	return exists(pkgjsonPath)
+const appsWithPkgJson = [...examples, ...apps].filter((app) => {
+  const pkgjsonPath = path.join(app, 'package.json')
+  return exists(pkgjsonPath)
 })
 
 // update the package.json file name property
@@ -44,7 +44,7 @@ const appsWithPkgJson = [...examples, ...apps].filter(app => {
 // name: "exercises__sep__01-goo.problem__sep__01-great"
 
 function relativeToWorkshopRoot(dir) {
-	return dir.replace(`${workshopRoot}${path.sep}`, '')
+  return dir.replace(`${workshopRoot}${path.sep}`, '')
 }
 
 await updatePkgNames()
@@ -53,96 +53,105 @@ await copyExerciseTsConfigs()
 await copyExercisePrettierConfigs()
 
 async function updatePkgNames() {
-	for (const file of appsWithPkgJson) {
-		const pkgjsonPath = path.join(file, 'package.json')
-		const pkg = JSON.parse(await fs.promises.readFile(pkgjsonPath, 'utf8'))
-		pkg.name = relativeToWorkshopRoot(file).replace(/\\|\//g, '__sep__')
-		const written = await writeIfNeeded(
-			pkgjsonPath,
-			`${JSON.stringify(pkg, null, 2)}\n`,
-		)
-		if (written) {
-			console.log(`updated ${path.relative(process.cwd(), pkgjsonPath)}`)
-		}
-	}
+  for (const file of appsWithPkgJson) {
+    const pkgjsonPath = path.join(file, 'package.json')
+    const pkg = JSON.parse(await fs.promises.readFile(pkgjsonPath, 'utf8'))
+    pkg.name = relativeToWorkshopRoot(file).replace(/\\|\//g, '__sep__')
+    const written = await writeIfNeeded(
+      pkgjsonPath,
+      `${JSON.stringify(pkg, null, 2)}\n`,
+    )
+    if (written) {
+      console.log(`updated ${path.relative(process.cwd(), pkgjsonPath)}`)
+    }
+  }
 }
 
 async function updateRootTsConfig() {
-	const tsconfig = {
-		files: [],
-		exclude: ['node_modules'],
-		references: appsWithPkgJson.map(a => ({
-			path: relativeToWorkshopRoot(a).replace(/\\/g, '/'),
-		})),
-	}
-	const written = await writeIfNeeded(
-		path.join(workshopRoot, 'tsconfig.json'),
-		`${JSON.stringify(tsconfig, null, 2)}\n`,
-		{ parser: 'json' },
-	)
+  const tsconfig = {
+    files: [],
+    exclude: ['node_modules'],
+    references: appsWithPkgJson.map((a) => ({
+      path: relativeToWorkshopRoot(a).replace(/\\/g, '/'),
+    })),
+  }
+  const written = await writeIfNeeded(
+    path.join(workshopRoot, 'tsconfig.json'),
+    `${JSON.stringify(tsconfig, null, 2)}\n`,
+    { parser: 'json' },
+  )
 
-	if (written) {
-		// delete node_modules/.cache
-		const cacheDir = path.join(workshopRoot, 'node_modules', '.cache')
-		if (exists(cacheDir)) {
-			await fs.promises.rm(cacheDir, { recursive: true })
-		}
-		console.log('all fixed up')
-	}
+  if (written) {
+    // delete node_modules/.cache
+    const cacheDir = path.join(workshopRoot, 'node_modules', '.cache')
+    if (exists(cacheDir)) {
+      await fs.promises.rm(cacheDir, { recursive: true })
+    }
+    console.log('all fixed up')
+  }
 }
 
 async function copyExerciseTsConfigs() {
-	const exercisesWithoutTsCongif = exerciseApps.filter((exercisePath) => {
-		return !fs.existsSync(path.join(exercisePath, 'tsconfig.json'))
-	})
+  const exercisesWithoutTsCongif = exerciseApps.filter((exercisePath) => {
+    return !fs.existsSync(path.join(exercisePath, 'tsconfig.json'))
+  })
 
-	if (exercisesWithoutTsCongif.length === 0) {
-		return
-	}
+  if (exercisesWithoutTsCongif.length === 0) {
+    return
+  }
 
-	const tsConfigTemplate = await fs.promises.readFile(
-		here('./tsconfig.json'),
-		'utf8',
-	)
+  const tsConfigTemplate = await fs.promises.readFile(
+    here('./tsconfig.json'),
+    'utf8',
+  )
 
-	await Promise.all(exercisesWithoutTsCongif.map((exercisePath) => {
-		const tsConfigPath = path.resolve(exercisePath, 'tsconfig.json')
-		return fs.promises.writeFile(tsConfigPath, tsConfigTemplate)
-	}))
+  await Promise.all(
+    exercisesWithoutTsCongif.map((exercisePath) => {
+      const tsConfigPath = path.resolve(exercisePath, 'tsconfig.json')
+      return fs.promises.writeFile(tsConfigPath, tsConfigTemplate)
+    }),
+  )
 }
 
 async function copyExercisePrettierConfigs() {
-	const prettierConfig = await fs.promises.readFile(
-		path.join(workshopRoot, '.prettierrc'),
-		'utf8',
-	)
-	await Promise.all(exerciseApps.map(exercise => {
-		const prettierConfigPath = path.join(exercise, '.prettierrc')
-		return fs.promises.writeFile(prettierConfigPath, prettierConfig)
-	}))
+  const rootPrettierFilePath = path.join(workshopRoot, '.prettierrc')
+  if (!fs.existsSync(rootPrettierFilePath)) {
+    return
+  }
+
+  const prettierConfig = await fs.promises.readFile(
+    rootPrettierFilePath,
+    'utf8',
+  )
+  await Promise.all(
+    exerciseApps.map((exercise) => {
+      const prettierConfigPath = path.join(exercise, '.prettierrc')
+      return fs.promises.writeFile(prettierConfigPath, prettierConfig)
+    }),
+  )
 }
 
 async function writeIfNeeded(filepath, content) {
-	const oldContent = await fs.promises.readFile(filepath, 'utf8')
-	if (oldContent !== content) {
-		await fs.promises.writeFile(filepath, content)
-	}
-	return oldContent !== content
+  const oldContent = await fs.promises.readFile(filepath, 'utf8')
+  if (oldContent !== content) {
+    await fs.promises.writeFile(filepath, content)
+  }
+  return oldContent !== content
 }
 
 function exists(p) {
-	if (!p) return false
-	try {
-		fs.statSync(p)
-		return true
-	} catch (error) {
-		return false
-	}
+  if (!p) return false
+  try {
+    fs.statSync(p)
+    return true
+  } catch (error) {
+    return false
+  }
 }
 
 async function readDir(dir) {
-	if (exists(dir)) {
-		return fs.promises.readdir(dir)
-	}
-	return []
+  if (exists(dir)) {
+    return fs.promises.readdir(dir)
+  }
+  return []
 }
