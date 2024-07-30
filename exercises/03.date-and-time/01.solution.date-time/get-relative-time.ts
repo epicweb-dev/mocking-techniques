f83b9296ba851bdacdc12dc2c7dee0bc9768b735
Artem Zakharchenko <kettanaito@gmail.com>
2024-07-30 15:37:25 +0200

    03/01 rewrite using Intl.RelativeTimeFormat

--- exercises/03.date-and-time/01.solution.date-time/get-relative-time.ts
+++ exercises/03.date-and-time/01.solution.date-time/get-relative-time.ts
@@ -5,37 +5,30 @@ export function getRelativeTime(date: Date): string {
   const delta =
     Math.floor(Date.now() / 1000) - Math.floor(date.getTime() / 1000)
 
-  const formatString = (value: number, unit: string): string => {
-    return new Intl.NumberFormat('en-US', {
-      style: 'unit',
-      unit,
-      unitDisplay: 'long',
-    }).format(value)
-  }
+  /**
+   * Use the standard `Intl.RelativeTimeFormat` API to produce human-friendly relative time strings.
+   * @see https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Intl/RelativeTimeFormat
+   */
+  const formatter = new Intl.RelativeTimeFormat('en', { style: 'long' })
 
   switch (true) {
     case delta < 60: {
       return `Just now`
     }
     case delta < 3600: {
-      const minutes = Math.floor(delta / 60)
-      return `${formatString(minutes, 'minute')} ago`
+      return formatter.format(-Math.floor(delta / 60), 'minute')
     }
     case delta < 86_400: {
-      const hours = Math.floor(delta / 3600)
-      return `${formatString(hours, 'hour')} ago`
+      return formatter.format(-Math.floor(delta / 3600), 'hour')
     }
     case delta < 2_620_800: {
-      const days = Math.floor(delta / 86_400)
-      return `${formatString(days, 'day')} ago`
+      return formatter.format(-Math.floor(delta / 86_400), 'day')
     }
     case delta < 31_449_600: {
-      const months = Math.floor(delta / 2_620_800)
-      return `${formatString(months, 'month')} ago`
+      return formatter.format(-Math.floor(delta / 2_620_800), 'month')
     }
     default: {
-      const years = Math.floor(delta / 31_449_600)
-      return `${formatString(years, 'year')} ago`
+      return formatter.format(-Math.floor(delta / 31_449_600), 'year')
     }
   }
 }
