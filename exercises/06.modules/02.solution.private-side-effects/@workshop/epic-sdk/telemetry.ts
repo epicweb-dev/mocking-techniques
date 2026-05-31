2abe8bc1bd0b973b8915ee6c4fba17e50c12364e
Vasilii Kovalev <vasilii.kovalev95@yandex.ru>
2026-05-31 15:20:54 +0500

    [06.modules.02.solution.private-side-effects] Include `null` type where necessary

--- exercises/06.modules/02.solution.private-side-effects/@workshop/epic-sdk/telemetry.ts
+++ exercises/06.modules/02.solution.private-side-effects/@workshop/epic-sdk/telemetry.ts
@@ -2,7 +2,7 @@ import { setTimeout } from 'node:timers/promises'
 
 export function initTelemetry() {
   return {
-    async record(event: string, data: Record<string, unknown>) {
+    async record(event: string, data: Record<string, unknown> | null) {
       await setTimeout(6000)
     },
   }
